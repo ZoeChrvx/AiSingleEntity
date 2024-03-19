de307fa0355c127dfb8bcb1b8c1e6644989c2556
ZoeChrvx <zchervaux@artfx.fr>
2024-03-19 13:38:18 +0100

    [fix] Dikjstra Done

--- AiSingleEntity/AiSingleEntity.cpp
+++ AiSingleEntity/AiSingleEntity.cpp
@@ -3,69 +3,83 @@
 #include <iostream>
 #include <ostream>
 #include <limits>
+#include <vector>
 
 int inf = std::numeric_limits<int>::max();
+int steps;
+constexpr int nodes_count = 7;
 using namespace std;
+
+void Result(int dist[])
+{
+    cout<<"Vertex\t\tDistance from source vertex"<< '\n';
+    for(int v = 0; v<nodes_count; v++)                      
+    { 
+        char str=65+v; 
+        cout<<str<<"\t\t\t"<<dist[v]<< '\n';
+    }
+}
 
 int miniDist(int distance[], bool Tset[]) // finding minimum distance
 {
     int minimum=INT_MAX;
-    int ind;
+    int minDist = 0;
               
-    for(int k=0;k<7;k++) 
+    for(int v=0;v<nodes_count;v++) 
     {
-        if(Tset[k]==false && distance[k]<=minimum)      
+        if(Tset[v]==false && distance[v]<=minimum)      
         {
-            minimum=distance[k];
-            ind=k;
+            minimum=distance[v];
+            minDist=v;
         }
     }
-    return ind;
+    return minDist;
 }
 
-void DijkstraAlgo(int graph[7][7],int src) // adjacency matrix 
+void Dijkstra(int graph[nodes_count][nodes_count], int src)
 {
-    int distance[7]; // // array to calculate the minimum distance for each node                             
-    bool Tset[7];// boolean array to mark visited and unvisited for each node
+    int dist[nodes_count];
+    bool prev[nodes_count];
     
-     
-    for(int k = 0; k<7; k++)
+    //Q = set
+    vector<int> set;
+    
+    for(int v = 0; v<nodes_count; v++)
     {
-        distance[k] = INT_MAX;
-        Tset[k] = false;    
+        dist[v] = INT_MAX;
+        prev[v] = false;
+        set.emplace_back(v);
+
     }
-    
-    distance[src] = 0;   // Source vertex distance is set 0               
-    
-    for(int k = 0; k<7; k++)                           
+    dist[src]=0;
+
+    for(int v=0; v< nodes_count; v++)
     {
-        int m=miniDist(distance,Tset); 
-        Tset[m]=true;
-        for(int k = 0; k<7; k++)                  
-        {
-            // updating the distance of neighbouring vertex
-            if(!Tset[k] && graph[m][k] && distance[m]!=INT_MAX && distance[m]+graph[m][k]<distance[k])
-                distance[k]=distance[m]+graph[m][k];
+        int m = miniDist(dist, prev);
+        prev[m]=true;
+        for(int v = 0; v<nodes_count;v++)
+        { 
+            if(!prev[v] && graph[m][v] && dist[m] != INT_MAX && dist[m]+graph[m][v] < dist[v])
+            {
+                dist[v]=dist[m]+graph[m][v];
+            }
         }
     }
-    cout<<"Vertex\t\tDistance from source vertex"<<endl;
-    for(int k = 0; k<7; k++)                      
-    { 
-        char str=65+k; 
-        cout<<str<<"\t\t\t"<<distance[k]<<endl;
-    }
+
+    Result(dist);
 }
+
 
 int main()
 {
-    int graph[7][7]={
+    int graph[nodes_count][nodes_count] = {
         {0, 10, 15, NULL, 30, NULL, NULL},
         {NULL, 0, NULL, NULL, NULL, 57, NULL},
         {15, NULL, 0, 16, NULL, NULL, 52},
         {NULL, NULL, 13, 0, NULL, NULL, NULL},
         {30, NULL, NULL, NULL, 0, 11, 34},
         {NULL, 49, NULL, NULL, 12, 0, NULL},
-        {NULL, NULL, 63, NULL, 35, NULL, 0}};
-    DijkstraAlgo(graph,0);
-    return 0;                           
+        {NULL, NULL, 63, NULL, 35, NULL, 0}
+    };
+    Dijkstra(graph,2);                        
 }
--- AiSingleEntity/Algorithm.h
+++ AiSingleEntity/Algorithm.h
@@ -3,19 +3,46 @@
 void Dijkstra(int graph[][], int nodesCount, int src)
 {
     int dist[nodesCount];
-    int prev[nodesCount][nodesCount];
+    int pred[nodesCount][nodesCount];
+    
+    //Q = set
     std::vector<int> set;
     
     for(int v = 0; v<nodesCount; v++)
     {
         dist[v] = INT_MAX;
-        prev[v] = {};
+        pred[v] = {};
         set.emplace_back(v);
     }
     dist[src]=0;
 
     while(!std::empty(set))
     {
-         
+        int minDist = INT_MIN;
+        int minNode = -1;
+
+        for(int v : set)
+        {
+            if(dist[v] < minDist)
+            {
+                minDist = dist[v];
+                minNode=v;
+            }
+        }
+
+        if(minNode == -1) break;
+
+        set.erase(std::remove(set.begin(), set.end(), minNode), set.end());
+        for(int v = 0;v<nodesCount;v++)
+        {
+            if(graph[minNode][v]!=0)
+            {
+                int alt = dist[minNode] + graph[minNode][v];
+                if(alt < dist[v])
+                {
+                    dist[v] = alt;
+                }
+            }
+        }
     }
 }
